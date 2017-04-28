c471deb1dd9de81c1b45d6b42cb7c86baf87055e
李佳蓁 <lijiazhen@lijiazhende-MacBook-Pro.local>
2017-04-28 15:58:46 +0800

    Initial Commit

---
+++ MyHW2_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyHW2_1
+//
+//  Created by 李佳蓁 on 2017/4/28.
+//  Copyright © 2017年 李佳蓁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
